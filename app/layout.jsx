e51aa82aa6a5edaa37e58cace4998c7f1ee181e1
Michael Nelles <mxnelles@gmail.com>
2023-06-21 08:52:15 -0500

    update components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,33 @@
 import "@styles/globals.css"; // no need to show full path just '@' will do
 
-const layout = () => {
-   return <div>layout</div>;
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+
+export const metadata = {
+   title: "AI Next.js application",
+   description: "AI Next.js application",
+   keywords: "AI Next.js application",
 };
 
-export default layout;
+const RootLayout = ({ children }) => {
+   return (
+      <html lang='en'>
+         <head>
+            <meta charSet='utf-8' />
+            <meta name='description' content={metadata.description} />
+            <meta name='keywords' content={metadata.keywords} />
+         </head>
+         <body>
+            <div className='main'>
+               <div className='gradient' />
+            </div>
+            <main className='app'>
+               <Nav />
+               {children}
+            </main>
+         </body>
+      </html>
+   );
+};
+
+export default RootLayout;
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,22 @@
+import Feed from "@components/Feed";
+
 const Home = () => {
-   return <div>page</div>;
+   return (
+      <section className='w-full flex-center flex-col'>
+         <h1 className='head_text text-center'>
+            Welcome
+            <br classNAme='max-md:hidden' />
+            <span className='orange_gradient text-center'>
+               AI powered Promps
+            </span>
+         </h1>
+         <p className='desc text-center'>
+            Open source AI sharing application for creative, technical and other
+         </p>
+
+         <Feed />
+      </section>
+   );
 };
 
-export default page;
+export default Home;
