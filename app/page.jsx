24bba6aaefb9fb18978d18aad4afe75c5e673084
Michael Nelles <mxnelles@gmail.com>
2023-06-22 09:44:36 -0500

    config logs and layout

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,55 @@
 import Feed from "@components/Feed";
+import Image from "next/image";
+
+const size = 50;
 
 const Home = () => (
    <section className='w-full flex-center flex-col'>
       <h1 className='head_text text-center'>
-         Discover & Share
+         AI Wall Post
          <br className='max-md:hidden' />
-         <span className='orange_gradient text-center'>
-            {" "}
-            AI-Powered Prompts
-         </span>
       </h1>
+      <div class='flex justify-center gap-2'>
+         <div>
+            <Image
+               src='/assets/images/chatLogo.png'
+               alt='logo'
+               width={size}
+               height={size}
+               className='object-contain rounded-full'
+            />
+         </div>
+         <div>
+            <Image
+               src='/assets/images/react2.png'
+               alt='logo'
+               width={size}
+               height={size}
+               className='object-contain rounded-full'
+            />
+         </div>
+         <div>
+            <Image
+               src='/assets/images/next2.png'
+               alt='logo'
+               width={size}
+               height={size}
+               className='object-contain rounded-full'
+            />
+         </div>
+         <div>
+            <Image
+               src='/assets/images/mongo2.png'
+               alt='logo'
+               width={size}
+               height={size}
+               className='object-contain rounded-full'
+            />
+         </div>
+      </div>
       <p className='desc text-center'>
-         NEXTAI is an open-source AI prompting tool for modern world to
-         discover, create and share creative prompts
+         Please share an insightful question you have posed to AI by asking it
+         here.
       </p>
 
       <Feed />
