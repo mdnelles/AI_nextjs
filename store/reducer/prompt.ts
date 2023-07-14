08d52c057674241a40bf8d1a798e1b78955dea2e
Michael Nelles <mxnelles@gmail.com>
2023-07-14 16:45:27 -0500

    update redux for user

--- store/reducer/prompt.ts
+++ store/reducer/prompt.ts
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
-import { Prompt, PromptState } from "types/prompt";
+import { Prompt, PromptState } from "@/types/prompt";
 
 const initialState: PromptState = {
    arr: [],
